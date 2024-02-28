2195a3d0d5d8c39c45d65e4a6f7b0b605b5bf596
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-02-28 16:10:25 -0300

    Added slug query endpoint

--- src/index.js
+++ src/index.js
@@ -19,4 +19,20 @@ app.get('/get/latest/:count', async (c) =>  {
   return c.json(posts.results, 200)
 })
 
+app.get('/get/slug/:id', async (c) => {
+  const slug = c.req.param('id')
+
+  const post = await c.env.DB.prepare(
+    `
+      SELECT *
+      FROM Pages
+      WHERE Slug = ?
+    `
+  ).bind(slug).all()
+
+  if (post.results.length == 0) return c.text("Not Found", 404)
+
+  return c.json(post.results, 200)
+})
+
 export default app
